d02f5920b39a9054546b9b629ebdb5a3878dde77
Sinchanabn <sinchanabn864@gmail.com>
2021-12-14 18:59:45 +0530

    Added Main meu and created Game scene

---
+++ Assets/Sripts/MainMenuManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public static int howManyPlayers;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void two_player()
+    {
+        SoundManager.buttonAudioSource.Play();
+        howManyPlayers = 2;
+        SceneManager.LoadScene("Ludo");
+    }
+
+    public void three_player()
+    {
+        SoundManager.buttonAudioSource.Play();
+        howManyPlayers = 3;
+        SceneManager.LoadScene("Ludo");
+    }
+
+    public void four_player()
+    {
+        SoundManager.buttonAudioSource.Play();
+        howManyPlayers = 4;
+        SceneManager.LoadScene("Ludo");
+    }
+
+    public void quit()
+    {
+        SoundManager.buttonAudioSource.Play();
+        Application.Quit();
+    }
+}
