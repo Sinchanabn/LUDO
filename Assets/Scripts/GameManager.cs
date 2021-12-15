c395eb3da6198b0cf45be7db9ad31dff2faf2282
Sinchanabn <sinchanabn864@gmail.com>
2021-12-15 19:51:38 +0530

    Added Dice Animations, winning screen UI,Quit menu and started working on Game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //variable to count the number of winners
+    private int totalBlueInHouse, totalRedInHouse, totalGreenInHouse, totalYellowInHouse;
+
+    //holds frames
+    public GameObject frameRed, frameGreen, frameBlue, frameYellow;
+    
+    //holds colorboard
+    public GameObject redPlayerI_Border, redPlayerII_Border, redPlayerIII_Border, redPlayerIV_Border;
+    public GameObject greenPlayerI_Border, greenPlayerII_Border, greenPlayerIII_Border, greenPlayerIV_Border;
+    public GameObject bluePlayerI_Border, bluePlayerII_Border, bluePlayerIII_Border, bluePlayerIV_Border;
+    public GameObject yellowPlayerI_Border, yellowPlayerII_Border, yellowPlayerIII_Border, yellowPlayerIV_Border;
+
+    //holds each players starts position
+    public Vector3 redPlayerI_Pos, redPlayerII_Pos, redPlayerIII_Pos, redPlayerIV_Pos;
+    public Vector3 greenPlayerI_Pos, greenPlayerII_Pos, greenPlayerIII_Pos, greenPlayerIV_Pos;
+    public Vector3 bluePlayerI_Pos, bluePlayerII_Pos, bluePlayerIII_Pos, bluePlayerIV_Pos;
+    public Vector3 yellowPlayerI_Pos, yellowPlayerII_Pos, yellowPlayerIII_Pos, yellowPlayerIV_Pos;
+
+    //Holds main and each colored dice positions
+    public Transform diceRoll;
+    public Transform redDiceRollPos, greenDiceRollPos, blueDiceRollPos, yellowDiceRollPos;
+
+    //holds player button when we click
+    public Button RedPlayerI_Button, RedPlayerII_Button, RedPlayerIII_Button, RedPlayerIV_Button;
+    public Button GreenPlayerI_Button, GreenPlayerII_Button, GreenPlayerIII_Button, GreenPlayerIV_Button;
+    public Button BluePlayerI_Button, BluePlayerII_Button, BluePlayerIII_Button, BluePlayerIV_Button;
+    public Button YellowPlayerI_Button, YellowPlayerII_Button, YellowPlayerIII_Button, YellowPlayerIV_Button;
+
+    public GameObject blueScreen, greenScreen, redScreen, yellowScreen;
+
+
+    private string playerTurn = "RED";
+    private string currentPlayer = "none";
+    private string currentPlayerName = "none";
+
+    //player movment controller
+    public GameObject redPlayerI, redPlayerII, redPlayerIII, redPlayerIV;
+    public GameObject greenPlayerI, greenPlayerII, greenPlayerIII, greenPlayerIV;
+    public GameObject bluePlayerI, bluePlayerII, bluePlayerIII, bluePlayerIV;
+    public GameObject yellowPlayerI, yellowPlayerII, yellowPlayerIII, yellowPlayerIV;
+
+
+    //holds each players steps
+    private int redPlayerI_Steps, redPlayerII_Steps, redPlayerIII_Steps, redPlayerIV_Steps;
+    private int greenPlayerI_Steps, greenPlayerII_Steps, greenPlayerIII_Steps, greenPlayerIV_Steps;
+    private int bluePlayerI_Steps, bluePlayerII_Steps, bluePlayerIII_Steps, bluePlayerIV_Steps;
+    private int yellowPlayerI_Steps, yellowPlayerII_Steps, yellowPlayerIII_Steps, yellowPlayerIV_Steps;
+
+    //selection of dice numbers animation
+    private int selectDiceNumAnimation;
+
+    //Dice Animation
+    public GameObject dice1_Roll_Animation;
+    public GameObject dice2_Roll_Animation;
+    public GameObject dice3_Roll_Animation;
+    public GameObject dice4_Roll_Animation;
+    public GameObject dice5_Roll_Animation;
+    public GameObject dice6_Roll_Animation;
+
+    //Players movement corenspoding to blocks
+    public List<GameObject> redMovementBlocks = new List<GameObject>();
+    public List<GameObject> greenMovementBlocks = new List<GameObject>();
+    public List<GameObject> yellowMovementBlocks = new List<GameObject>();
+    public List<GameObject> blueMovementBlocks = new List<GameObject>();
+
+    // Random generation of dice numbers
+    private System.Random randomNo;
+    public GameObject confirmScreen;
+    public GameObject gameCompletedScreen;
+
+    public Button DiceRollButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
